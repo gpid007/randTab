d750838685d05323671451576d407989ff64cbc1
gpid007 <heine.gregor@gmail.com>
2019-04-07 23:42:43 +0200

    open images

--- background_script.js
+++ background_script.js
@@ -5,11 +5,24 @@ console.log(tab);
         chrome.tabs.getAllInWindow(tab.windowId, function (tabs) {
         console.log(tabs);
 
-            // Random integer formula
+            // Random integer formula for tab selection
             var ix = Math.floor(Math.random() * tabs.length);  
             console.log(ix);
+
             chrome.tabs.update(tabs[ix].id, {selected: true});
-            console.log('OK');
+            console.log(window.location.href);
+
+            // Open all images of selected (parent) tab
+            var re0 = /.*big\.jpg/;
+            var tagList = document.getElementsByTagName('a');
+            console.log(tagList);
+
+            for (var i=0; i<tagList.length; i++) {
+                if ( re0.test(tagList[i].href) ) {
+                    console.log(i, 'OK', tagList[i].href);
+                    window.open(tagList[i].href, '_blank');
+                }
+            }
 
         });
 
