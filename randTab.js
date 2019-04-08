8b829d642ba431f561c231385ad07e6fe1b63216
gpid007 <heine.gregor@gmail.com>
2019-04-08 13:34:57 +0200

    injection

--- randTab.js
+++ randTab.js
@@ -15,7 +15,28 @@ console.log(tab);
             console.log(tabs[ix].url);
             // window.open(tabs[ix].url);
 
-            // Open pics
+            chrome.tabs.executeScript(tabs[ix].id, {file: "inject.js"});
+            console.log("script injected");
+
+        });
+});
+
+/*
+You have only included jQuery on your background page. You should be injecting jQuery onto the tab you're executing script.js on.
+
+Here is an example that loads jquery.js first, then in a callback loads script.js:
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if(changeInfo && changeInfo.status == "complete"){
+        chrome.tabs.executeScript(tabId, {file: "jquery.js"}, function(){
+            chrome.tabs.executeScript(tabId, {file: "script.js"});
+        });
+    }
+});
+
+*/
+
+/*            // Open pics
             var imageList = $('li.thumbwook').find('a.rel-link');
             for (var i=0; i<imageList.length; i++) {
                 if ( imageList[i].getAttribute('data-size') != null ) {
@@ -24,16 +45,25 @@ console.log(tab);
                 }
             }
 
-/*
+
             chrome.tabs.executeScript(tab[ix].id, {
                 "file": "openPic.js"
             }, function () { 
                 console.log("Script Executed .. "); 
             });
 */
-        });
-});
 
 /*  CYCLE: ternary index condition for cycling: increase to max; then reset 
  *      var ix = (currentTab.index+1 < tabs.length) ? currentTab.index+1 : 0;
  */
+
+
+/*
+https://stackoverflow.com/questions/16302593/get-the-current-tab-in-jquery-ui-tabs
+According to manual http://api.jqueryui.com/tabs/ getter of active JqueryUI tab is
+
+var active = $( ".selector" ).tabs( "option", "active" );
+*Replace ".selector" by your one.
+
+Then active.attr( 'id' ) will return exactly what you need.
+*/
