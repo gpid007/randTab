8d72b8df7a619dbf68944a5a8a816df583a64304
gpid007 <heine.gregor@gmail.com>
2019-04-22 00:37:08 +0200

    randTab; change color; open first image

--- inject.js
+++ inject.js
@@ -1,10 +1,10 @@
-(function() {
-    // Open pics
-    var imageList = $('li.thumbwook').find('a.rel-link');
-    for (var i=0; i<imageList.length; i++) {
-        if ( imageList[i].getAttribute('data-size') != null ) {
-            window.open(imageList[i].getAttribute('href'), '__blank');
-            console.log(imageList[i].getAttribute('href'));
-        }
+
+document.body.style.background = '#cccccc';
+
+var imageList = $('li.thumbwook').find('a.rel-link');
+for (var i=0; i<imageList.length; i++) {
+    if ( imageList[i].getAttribute('data-size') != null ) {
+        window.open(imageList[i].getAttribute('href'), '__blank');
+        console.log(imageList[i].getAttribute('href'));
     }
-})();
+}
--- randTab.js
+++ randTab.js
@@ -8,8 +8,14 @@ chrome.browserAction.onClicked.addListener(function (tab) {
         var ix = Math.floor(Math.random() * tabs.length);  
         console.log('Math.floor(Math.random() * tabs.length)', ix);
 
-        chrome.tabs.update(tabs[ix].id, {selected: true, active: true});          
+        chrome.tabs.update(tabs[ix].id, {selected: true, active: true}); //, url:tabs[ix].url});          
         console.log('chrome.tabs.update(tabs[ix].id', tabs[ix].url);
+
+        // chrome.tabs.executeScript({file: 'inject.js'});
+        
+        chrome.tabs.executeScript(null, {file: "jquery-3.4.0.min.js"}, function() {
+            chrome.tabs.executeScript(null, {file: "inject.js"});
+        });
 
     });
 
